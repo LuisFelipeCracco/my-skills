539b11572d7e8e7bbebce47cd732f9be4bf029a2
LuisFelipeCracco <73305528+LuisFelipeCracco@users.noreply.github.com>
2022-01-31 15:30:10 -0300

    components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Button } from '../components/Button';
 
 export default function Home() {
 
@@ -21,17 +22,13 @@ export default function Home() {
         placeholderTextColor="#555"
         onChangeText={setNewSkill}
       />
-      <TouchableOpacity 
-        style={styles.button}
-        activeOpacity={0.7}
-        onPress={handleAddNewSkill}
-      >
-        <Text style={styles.buttonText}>ADD</Text>  
-      </TouchableOpacity> 
+       
 
       <Text style={[styles.title, {marginTop:20}]}>
         My Skills:
       </Text>
+
+     <Button />
 
       {
         mySkills.map(skill => (
@@ -68,18 +65,6 @@ const styles = StyleSheet.create({
     padding: 15,
     marginTop:30,
     borderRadius: 7,
-  },
-  button: {
-    backgroundColor: '#a370f7',
-    padding: 12,
-    borderRadius: 7,
-    alignItems: 'center',
-    marginTop: 20,
-  }, 
-  buttonText: {
-    color: '#fff',
-    fontSize: 17,
-    fontWeight: 'bold',
   },
   buttonSkill: {
     backgroundColor: '#1f1e25',
