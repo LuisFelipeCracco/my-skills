8f7336d522efb29d166af83d7747b58be7300979
LuisFelipeCracco <73305528+LuisFelipeCracco@users.noreply.github.com>
2022-02-01 17:32:35 -0300

    statusBar

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { StyleSheet, Text, View, TextInput, FlatList} from 'react-native';
+import { StyleSheet, Text, View, TextInput, FlatList, } from 'react-native';
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
